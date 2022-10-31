3f9c400e55a80c71f8a59fc1c89b33c27ff1d507
Niloofarhp <niloofar.hp@gmail.com>
2022-10-31 14:27:02 -0400

    initialize classes

---
+++ AdversarialTrain.py
@@ -0,0 +1,12 @@
+class AdversarialPGD_Attack():
+  def __init__(self):
+    pass
+class AdversarialTrain():
+  def __init__(self):
+    pass
+  
+
+def main():
+  return
+if __name__ == '__main__':
+  main()
